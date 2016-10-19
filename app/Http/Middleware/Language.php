d86f38798ad5bc28e0933e3b60e9aec05a027a39
Pol Cámara <polcamara@soft10.es>
2016-10-19 15:44:16 +0100

    Make sure that outside the home page the locale is forced to english

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -1,4 +1,6 @@
-<?php namespace App\Http\Middleware;
+<?php
+
+namespace App\Http\Middleware;
 
 use Closure;
 use Cookie;
@@ -7,11 +9,11 @@ use App;
 use Illuminate\Routing\Redirector;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
-use Illuminate\Contracts\Routing\Middleware;
 
-class Language {
-
-    public function __construct(Application $app, Redirector $redirector, Request $request) {
+class Language
+{
+    public function __construct(Application $app, Redirector $redirector, Request $request)
+    {
         $this->app = $app;
         $this->redirector = $redirector;
         $this->request = $request;
@@ -19,12 +21,13 @@ class Language {
 
     /**
      *  We handle the web request and setup language.
-     *  We go from default setting, and see if we can update it with browser lang, 
+     *  We go from default setting, and see if we can update it with browser lang,
      *  cookie lang and finally url lang. Note: Default and browser lang are the ones
      *  with less precedence, and url on top.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
@@ -34,23 +37,31 @@ class Language {
         $browserLang = substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
         $locale = Config::get('app.locale');
 
-        if (!empty($browserLang) AND in_array($browserLang, Config::get('app.languages')))
+        if (!empty($browserLang) and in_array($browserLang, Config::get('app.languages'))) {
             $locale = $browserLang;
+        }
 
-        if (!empty($cookieLang) AND in_array($cookieLang, Config::get('app.languages')))
+        if (!empty($cookieLang) and in_array($cookieLang, Config::get('app.languages'))) {
             $locale = $cookieLang;
+        }
 
-        if (!empty($urlLang) AND in_array($urlLang, Config::get('app.languages')))
+        if (!empty($urlLang) and in_array($urlLang, Config::get('app.languages'))) {
             $locale = $urlLang;
+        }
 
-        App::setLocale($locale);
+        // This is just for the beta
+        if ($request->url() != url()->to('/')) {
+            App::setLocale('en');
+        } else {
+            App::setLocale($locale);
+        }
 
         if ($request->hasCookie('locale') && $cookieLang == $locale) {
-            return $next($request);    
-        } else {            
+            return $next($request);
+        } else {
             $response = $next($request);
+
             return $response->withCookie(cookie()->forever('locale', $locale));
         }
     }
-
 }
