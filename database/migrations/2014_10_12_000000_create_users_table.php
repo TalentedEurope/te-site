5fbbcd0fd0031fe0416499939f52d3bb1445fb93
Pol Cámara <polcamara@soft10.es>
2016-10-23 21:44:45 +0100

    Fix student table seeder so students appear on searches

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -29,7 +29,7 @@ class CreateUsersTable extends Migration
             $table->integer('userable_id');
             $table->string('userable_type');
             $table->boolean('notify_me')->default(true);
-            $table->boolean('visible')->default(false);
+            $table->boolean('visible')->default(true);
             $table->boolean('is_filled')->default(false);
             $table->rememberToken();
             $table->timestamps();
