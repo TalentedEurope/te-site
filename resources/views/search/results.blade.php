6c574cc9fad11974839417c34fa5f672d6811272
Pol Cámara <polcamara@soft10.es>
2016-10-25 22:08:28 +0100

    Lighten companies/students when on its page

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -1,11 +1,17 @@
 @extends('../layouts.app')
 
 @section('page-title') Search for @endsection
-@section('page-class') search students @endsection
+@section('page-class') search students companies @endsection
 @section('meta')
   <meta id="token" content="{{ $token }}" />
   <meta id="user_type" content="{{ $userType }}" />
 @endsection
+@if ($type == 'students')
+  @section('menu-student') class="active" @endsection
+@endif
+@if ($type == 'companies')
+  @section('menu-company') class="active" @endsection
+@endif
 
 @section('content')
 <div class="container v-container">
