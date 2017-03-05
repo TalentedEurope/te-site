78a32409361494990c9fd387d4a3a44ed4c7823d
Carlos Sosa <sosahcarlos@gmail.com>
2017-03-05 22:17:38 +0000

    Add author credits of some icons

--- resources/views/static/terms.blade.php
+++ resources/views/static/terms.blade.php
@@ -7,58 +7,73 @@
   <div class="row">
     <div class="col-md-12 col-xs-12">
       <h2 class="page-title">Website usage terms and conditions</h2>
-        <div class="well">
-          <p>Welcome to our website. If you continue to browse and use this website, you are agreeing to comply with and be bound by the following terms and conditions of use, which together with our privacy policy govern Talented Europe’s relationship with you in relation to this website. If you disagree with any part of these terms and conditions, please do not use our website.</p>
+      <div class="well">
+        <p>Welcome to our website. If you continue to browse and use this website, you are agreeing to comply with and be bound by the following terms and conditions of use, which together with our privacy policy govern Talented Europe’s relationship with you in relation to this website. If you disagree with any part of these terms and conditions, please do not use our website.</p>
 
-          <p>The term ‘Talented Europe’ or ‘us’ or ‘we’ refers to the owner of the website. Talented Europe is a KA203 Erasmus+ project lead for the CIFP César Manrique institution in Santa Cruz de Tenerife/Spain <a href="http://www.cifpcesarmanrique.es">(www.cifpcesarmanrique.es)</a>.The term ‘you’ refers to the user or viewer of our website.</p>
+        <p>The term ‘Talented Europe’ or ‘us’ or ‘we’ refers to the owner of the website. Talented Europe is a KA203 Erasmus+ project lead for the CIFP César Manrique institution in Santa Cruz de Tenerife/Spain <a href="http://www.cifpcesarmanrique.es">(www.cifpcesarmanrique.es)</a>.The term ‘you’ refers to the user or viewer of our website.</p>
 
-          <p>The use of this website is subject to the following terms of use:</p>
+        <p>The use of this website is subject to the following terms of use:</p>
+        <ul>
+          <li>The content of the pages of this website is for your general information and use only. It is subject to change without notice.</li>
+          <li>This website uses cookies to monitor browsing preferences.<br/><br/>If you do allow cookies to be used, the following personal information may be stored by us for use by third parties:
           <ul>
-            <li>The content of the pages of this website is for your general information and use only. It is subject to change without notice.</li>
-            <li>This website uses cookies to monitor browsing preferences.<br/><br/>If you do allow cookies to be used, the following personal information may be stored by us for use by third parties:
-            <ul>
-            <li>For Educational Institutions:<ul>
-            <li>Name.</li>
-            <li>Type.</li>
-            <li>Email.</li>
-            <li>Person in charge.</li>
-            <li>Fiscal-ID.</li>
-            <li>Address.</li>
-            <li>Country.</li></ul></li>
-            <li>For Companies:<ul>
-            <li>Name.</li>
-            <li>Email.</li>
-            <li>Fiscal-ID.</li>
-            <li>Address.</li>
-            <li>Country.</li>
-            <li>Activity sector.</li>
-            <li>Person in charge.</li></ul></li>
-            <li>For Students and Graduates:<ul>
-            <li>Name.</li>
-            <li>Surname.</li>
-            <li>Nationality.</li>
-            <li>Date of birth.</li>
-            <li>Phone.</li>
-            <li>Address.</li>
-            <li>Completed studies.</li>
-            <li>Institution</li>
-            <li>Type of studies (European Frame).</li>
-            <li>Europass curriculum.</li>
-            <li>Grade card.</li></ul></li>
-            <li>For Teachers (role of Referee)<ul>
-            <li>Institution.</li>
-            <li>Name.</li>
-            <li>Surname.</li>
-            <li>Email.</li>
-            <li>Department.</li>
-            <li>Position.</li></ul></li>
+            <li>For Educational Institutions:
+              <ul>
+                <li>Name.</li>
+                <li>Type.</li>
+                <li>Email.</li>
+                <li>Person in charge.</li>
+                <li>Fiscal-ID.</li>
+                <li>Address.</li>
+                <li>Country.</li>
+              </ul>
+            </li>
+            <li>For Companies:
+              <ul>
+                <li>Name.</li>
+                <li>Email.</li>
+                <li>Fiscal-ID.</li>
+                <li>Address.</li>
+                <li>Country.</li>
+                <li>Activity sector.</li>
+                <li>Person in charge.</li>
+              </ul>
+            </li>
+            <li>For Students and Graduates:
+              <ul>
+                <li>Name.</li>
+                <li>Surname.</li>
+                <li>Nationality.</li>
+                <li>Date of birth.</li>
+                <li>Phone.</li>
+                <li>Address.</li>
+                <li>Completed studies.</li>
+                <li>Institution</li>
+                <li>Type of studies (European Frame).</li>
+                <li>Europass curriculum.</li>
+                <li>Grade card.</li>
+              </ul>
+            </li>
+            <li>For Teachers (role of Referee)
+              <ul>
+                <li>Institution.</li>
+                <li>Name.</li>
+                <li>Surname.</li>
+                <li>Email.</li>
+                <li>Department.</li>
+                <li>Position.</li>
+              </ul>
+            </li>
             <li>Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.</li>
             <li>Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through this website meet your specific requirements.</li>
             <li>This website contains material which is owned by or licensed to us. This material includes, but is not limited to, the design, layout, look, appearance and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these terms and conditions.</li>
             <li>All trademarks reproduced in this website, which are not the property of, or licensed to the operator, are acknowledged on the website.</li>
             <li>Unauthorised use of this website may give rise to a claim for damages and/or be a criminal offence.</li>
             <li>From time to time, this website may also include links to other websites. These links are provided for your convenience to provide further information. They do not signify that we endorse the website(s). We have no responsibility for the content of the linked website(s).</li>
-        </div>
+          </ul>
+        </ul>
+        <p>Some icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></p>
+      </div>
     </div>
   </div>
 </div>
