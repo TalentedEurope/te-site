49ce1b15c720f8d5ac8525b9ea2a6dcc555c3734
Carlos Sosa <sosahcarlos@gmail.com>
2017-04-04 23:04:28 +0100

    Fix cookies and privacy policy tabs

--- resources/views/static/cookies.blade.php
+++ resources/views/static/cookies.blade.php
@@ -7,32 +7,35 @@
   <div class="row">
     <div class="col-md-12 col-xs-12">
       <h2 class="page-title">Information about cookies</h2>
-        <div class="well">
+      <div class="well">
         <p>
-        According with Law 34/2002 about Services of the Information Society, we inform you that this website uses cookies.
+          According with Law 34/2002 about Services of the Information Society, we inform you that this website uses cookies.
         </p>
 
         <p>
-        Cookies are small text files used by the website you are visiting and they are stored on your computer. They are widely used in order to make the website work right and efficiently, since it provides information to the owners of the site. The use of cookies is standardized in the most of the websites. If you refuse to send cookies to our systems, you can “disable” and manage from your browser, removing them from your browsing history (cache) when the visit ends.</p>
+          Cookies are small text files used by the website you are visiting and they are stored on your computer. They are widely used in order to make the website work right and efficiently, since it provides information to the owners of the site. The use of cookies is standardized in the most of the websites. If you refuse to send cookies to our systems, you can “disable” and manage from your browser, removing them from your browsing history (cache) when the visit ends.
+        </p>
 
         <h3>Types of cookies</h3>
         <ul>
-        <li>“Session” cookies: they stay in your browser during your visit (for instance, till you close the browser and the visit ends).</li>
-        <li>“Persistent” cookies: they stay in your browser after the session (in case you do not remove them).</li>
-        <li>“Performance” cookies: they collect information about the use of the website; they do not collect personal information and collected data is stored in an anonymous way. The “Performance” cookies are used to improve the behaviour of the website.</li>
-        <li>“Functionality” cookies: they let to the website to store whichever option you have done in the page (for instance, changes in font- size, customization of pages) or let services as include posts in blogs.</li>
+          <li>“Session” cookies: they stay in your browser during your visit (for instance, till you close the browser and the visit ends).</li>
+          <li>“Persistent” cookies: they stay in your browser after the session (in case you do not remove them).</li>
+          <li>“Performance” cookies: they collect information about the use of the website; they do not collect personal information and collected data is stored in an anonymous way. The “Performance” cookies are used to improve the behaviour of the website.</li>
+          <li>“Functionality” cookies: they let to the website to store whichever option you have done in the page (for instance, changes in font- size, customization of pages) or let services as include posts in blogs.</li>
         </ul>
 
         <h3>Purpose</h3>
         <ul>
-        <li>Analytic cookies: they are used to collect statistics about the activity of the user. Among others, items such as followings are analysed: number of users who visit the website, number of visited pages as well as the activity of the users in the website and frequency of using. Collected information is always anonymous, therefore there will be no way to stablish a link between the information and the person whom it refers.</li>
-        <li>Cookies used by social networks: They let the user to have the chance of sharing with his contacts in a social network, the contents of interest through clicking the corresponding button (plug-in), which is inserted on the website. Plug-ins store and access to the cookies within the final user and let the social network to identify the users when these ones interact with the plug-ins.</li>
+          <li>Analytic cookies: they are used to collect statistics about the activity of the user. Among others, items such as followings are analysed: number of users who visit the website, number of visited pages as well as the activity of the users in the website and frequency of using. Collected information is always anonymous, therefore there will be no way to stablish a link between the information and the person whom it refers.</li>
+          <li>Cookies used by social networks: They let the user to have the chance of sharing with his contacts in a social network, the contents of interest through clicking the corresponding button (plug-in), which is inserted on the website. Plug-ins store and access to the cookies within the final user and let the social network to identify the users when these ones interact with the plug-ins.</li>
         </ul>
         <h3>Use of cookies Talented Europe</h3>
         <p>
-        Based on the directives of the National Spanish Agency of Data Protection, next is detailed the use of the cookies in this website. With the use of this website, you are conscious and agree that we store these cookies in your computer / device / browser for the declared aims.</p>
+          Based on the directives of the National Spanish Agency of Data Protection, next is detailed the use of the cookies in this website. With the use of this website, you are conscious and agree that we store these cookies in your computer / device / browser for the declared aims.
+        </p>
         <p>
-        Third-party cookies are those that are sent to the final machine of the user, from a host or domain that is not managed by the editor, but by another entity which treats the obtained data through the cookies. The next table explains the way we use the cookies:</p>
+          Third-party cookies are those that are sent to the final machine of the user, from a host or domain that is not managed by the editor, but by another entity which treats the obtained data through the cookies. The next table explains the way we use the cookies:
+        </p>
 
        <table class="table table-striped">
           <thead>
@@ -65,16 +68,11 @@
           </tbody>
         </table>
 
-
         <h3>Additional notes</h3>
         <p>Web browsers are the tools in charge of storing the cookies, and from this place you can exercise your rights in order to remove or disable them. Nor this web nor their legal representatives can guarantee the correct or incorrect handling of the cookies by the mentioned browsers.</p>
         <p>In some cases it is necessary to install cookies for the browser not to forget its decision of non-acceptance of the cookies.</p>
-        </div>
+      </div>
     </div>
   </div>
 </div>
 @endsection
-
-
-
-
