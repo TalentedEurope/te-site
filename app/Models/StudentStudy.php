ce11d6457b3cb6f0355f421a27ac9926e573551d
Carlos Sosa <sosahcarlos@gmail.com>
2017-04-05 13:33:53 +0100

    Add fashion-clothing field

--- app/Models/StudentStudy.php
+++ app/Models/StudentStudy.php
@@ -14,7 +14,7 @@ class StudentStudy extends Model
     );
 
     public static $levels = ['studies_level_5', 'studies_level_6', 'studies_level_7', 'studies_level_8'];
-    public static $fields = ['business-studies-and-or-management-science', 'engineering-and-or-technology', 'social-sciences', 'languages-and-philological-sciences', 'communication-and-information-sciences', 'humanities', 'education-and-or-teacher-training', 'law', 'art-and-design', 'architecture-and-or-urban-and-regional-planning', 'natural-sciences', 'mathematics-and-or-informatics', 'medical-sciences', 'geography-and-or-geology', 'agriculture-sciences'];
+    public static $fields = ['fashion-clothing', 'business-studies-and-or-management-science', 'engineering-and-or-technology', 'social-sciences', 'languages-and-philological-sciences', 'communication-and-information-sciences', 'humanities', 'education-and-or-teacher-training', 'law', 'art-and-design', 'architecture-and-or-urban-and-regional-planning', 'natural-sciences', 'mathematics-and-or-informatics', 'medical-sciences', 'geography-and-or-geology', 'agriculture-sciences'];
 
     public function student()
     {
